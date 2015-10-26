7fa6ac4816d796e2d9aaf1c5b534f13b22d59c75
Marcus Pan <marcuspanje@gmail.com>
2015-10-26 18:09:23 -0400

    some errors still

--- plot_phased_array.m
+++ plot_phased_array.m
@@ -12,96 +12,58 @@
 %Output S: signal ampl  according to coords,
 %S_angle: signal ampl according to angle
 
-function [S, S_angle] = plot_phased_array(posX, posY, posZ, fund_f, delay, theta_peak, td)
+function [S, S_angle] = plot_phased_array(posX, posY, posZ, Delay_i, xlen, xstep, zlen, zstep, sig, Fs, theta_label, td)
+
 %scale of model in mm
-xstep = 10; zstep = 10;
-v = 340e3;%speed of sound in mm/s
-X = -200:xstep:200; 
-Z = 0:zstep:1000;
-nx = numel(X); nz = numel(Z);
+
+v = 340e3; %speed of sound in mm/s
+
+X = -xlen/2:xstep:xlen/2;
+Z = 0:zstep:zlen;
+nx = numel(X); 
+nz = numel(Z);
 S = zeros(nx, nz);
 
 %polar plot
 theta_step = 0.001;
-theta = 0:theta_step:2*pi-theta_step;
-ntheta = numel(theta);
-%values of signal strength along a line from an angle from center
-rho = -1*ones(ntheta, nx*nz+1); %make each value -2 to  
+Theta = 0:theta_step:2*pi;
+ntheta = numel(Theta);
 
-%generate signal
-Fsig = 100;
-Fs = 300000;
-t = 0:1/Fs:1;
-len = max(size(t));
-half_len = round(ceil(len/2));
+S_angle = zeros(1, ntheta);
+Angle_hits = zeros(1, ntheta);
 
-base_signal = 1.5*cos(2*pi*Fsig*t);
-
-%pulse width modulation
-Fswth = fund_f;
-t_off = 0;
-swth = 2.5*sawtooth(2*pi*Fswth*t) + 2.5;
-
-%pwm(input, sawtooth, high, Vdc, fout, Fs, max_dutycycle)
-%Vdc is offset voltage added on to input
-Vdc = 2.5;
-Xpwm = pwm(base_signal, swth, 5.0, Vdc, Fswth, Fs, 0.99);
-figure();
-subplot(2, 1, 1);
-plot(t, base_signal + Vdc, t, swth, t, Xpwm);
-xlabel('t');
-ylabel('V');
-
-FX = fft(Xpwm);
-FX = abs(FX)/len;
-FX = 2*FX(1:half_len);
-w = linspace(0, Fs/2, half_len);
-
-subplot(2, 1, 2);
-plot(w, FX);
-xlabel('f');
-ylabel('M');
+signal_len = max(size(sig));
+signal_len_half = round(signal_len/2);
 
 %measure signals across the y = 0, xz plane
 x = 1; z = 1;
-for i = X(1) : xstep : X(nx)
-    for k = Z(1) : zstep : Z(nz)
-        radiuses = sqrt( (posX-i).^2 + (posY).^2 + (posZ-k).^2 ); 
+for i = 1:nx
+    for k = 1:nz
+        radiuses = sqrt( (posX-X(i)).^2 + (posY).^2 + (posZ-Z(k)).^2 ); 
         t = radiuses/v;
+
         %convert time to discrete indices
         ind = round(t*Fs);
-        delay_ind = round(delay*Fs); 
 
-        signals = Xpwm(round(1 + mod(ind - delay_ind, signal_len)));
-        S(x, z) = abs(sum(sum(signals)));%sig strength at a point
+        signals = sig(signal_len_half - ind - Delay_i);
+        S(i, k) = abs(sum(sum(signals)));%sig strength at a point
         
-        %compute angle from center and add sig strength to list of 
-        %sig strengths from that angle
-        angle = mod(2*pi + atan2(k, i), 2*pi); 
-        [M, I] = min(abs(theta-angle));
-
-        %NaN values are -1. store values-1 and adjust to 0 later
-        %so nnz() can be used to find NaN values.
-        curr_index = nnz(rho(I, :) + 1);
-        rho(I, curr_index + 1) = S(x, z) - 1; 
-
-        z = z+1;
+        %compute angle from center, and get index of closest discretized angle.
+        %add signal strength at the point to signal strength at that angle.
+        %increase Angle_hits at that angle. This will be used to compute the 
+        %mean signal strength at an angle, as not every angle will get an equal number of hits
+        angle = atan2(Z(k), X(i)); 
+        angle_i = mod(1 + round(angle*ntheta/(2*pi)), ntheta);
+        S_angle(angle_i) = S_angle(angle_i) + S(i, k);
+        Angle_hits(angle_i) = Angle_hits(angle_i) + 1;
     end
-    x = x+1;
-    z = 1;
 end
 
 S_normalized = S./max(max(S));
 
 %get mean of sig strength at every angle.
-%-1 values are NaN, so +1 and use nnz() to disregard zero values
-rho1 = rho + 1;
-S_angle = zeros(1, ntheta);
-
-for i = 1:ntheta
-    sigs_alng_line = rho1(i, :);
-    S_angle(i) = sum(sigs_alng_line)/nnz(sigs_alng_line); 
-end
+S_angle = S_angle./Angle_hits;
+S_angle_normalized = S_angle./max(S_angle);
 
 figure('Position', [100 100 1200 500]);
 planeplot = subplot(1, 2, 1);
@@ -111,8 +73,7 @@ view(-90, 90);
 zhandle = colorbar;
 
 angularplot = subplot(1, 2, 2);
-S_angle = S_angle./max(S_angle);
-polar(theta, S_angle, '.');
-str_title = sprintf('Signal strength map:\n phiPeak = %d deg, td = %.5f ms', theta_peak, td*1000)
+polar(Theta, S_angle_normalized, '.');
+str_title = sprintf('Signal strength map:\n angle = %d deg, td = %.5f ms', theta_label, td*1000);
 title(planeplot, str_title, 'FontSize', 20);
 
--- vert_speaker_phased_array.m
+++ vert_speaker_phased_array.m
@@ -1,5 +1,6 @@
-f = 40000; %frequency = 40 kHz
-lambda = 1000*340/f; %wavelength in mm;
+%----------------------
+%generate speaker matrix
+%----------------------
 
 %coordinates of X, Y positions from center in mm
 numx = 5; numy = 8;
@@ -11,13 +12,67 @@ Y1 = transpose(linspace(-59.5, 59.5, numy));
 posY = [Y1 Y1 Y1 Y1 Y1];
 posZ = zeros(numy, numx);
 
+%----------------------
+%generate signal
+%----------------------
+Fsig = 1000;
+Fs = 300000;
+Fswth = 40000;
+
+t = 0:1/Fs:1;
+len = max(size(t));
+half_len = round(ceil(len/2));
+
+base_signal = 1.5*cos(2*pi*Fsig*t);
+
+%pulse width modulation
+t_off = 0;
+swth = 2.5*sawtooth(2*pi*Fswth*t) + 2.5;
+
+%pwm(input, sawtooth, high, Vdc, fout, Fs, max_dutycycle)
+%Vdc is offset voltage added on to input
+Vdc = 2.5;
+Xpwm = pwm(base_signal, swth, 5.0, Vdc, Fswth, Fs, 0.99);
+%figure();
+%subplot(2, 1, 1);
+%plot(t, base_signal + Vdc, t, swth, t, Xpwm);
+%xlabel('t');
+%ylabel('V');
+
+%FX = fft(Xpwm);
+%FX = abs(FX)/len;
+%FX = 2*FX(1:half_len);
+%w = linspace(0, Fs/2, half_len);
+
+%subplot(2, 1, 2);
+%plot(w, FX);
+%xlabel('f');
+%ylabel('M');
+
+%----------------------
+%generate delay matrix
+%----------------------
+
 %theta is desired degree of beam from normal
-theta = 30;
+theta = 15;
 td = (posX(2)-posX(1))*sind(theta)/(1000*340); 
+
 %td = 10.851E-6;
 delay1 = [0 2*td 4*td 6*td 8*td];
 delay2 = [td 3*td 5*td 7*td 9*td];
 delay = [delay1;delay2;delay1;delay2];
 delay = [delay;delay];
+
+%convert to discrete units
+Delay_i = delay * Fs;
+
+%----------------------
+%plot_phased_array
+%---------------------
+xlen = 400;
+xstep = 10;
+zlen = 1000;
+zstep = 10;
  
-plot_phased_array(posX, posY, posZ, f, delay, theta, td);
+plot_phased_array(posX, posY, posZ, Delay_i, xlen, xstep, zlen, zstep, Xpwm, Fs, theta, td);
+
