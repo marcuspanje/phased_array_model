e8be9a98bedef5ff6e5f7b7e7e861a9339e14d6e
marcuspanje <marcuspanje@gmail.com>
2015-11-18 14:24:52 -0500

    added code generator

---
+++ delay_code.v
@@ -0,0 +1,291 @@
+//delay code for negative angles
+
+//delay for angle -30
+signal[0] <= delay_w[8500];
+signal[1] <= delay_w[8070];
+signal[2] <= delay_w[7640];
+signal[3] <= delay_w[7210];
+signal[4] <= delay_w[6780];
+signal[5] <= delay_w[6350];
+signal[6] <= delay_w[5920];
+signal[7] <= delay_w[5490];
+signal[8] <= delay_w[5060];
+signal[9] <= delay_w[4630];
+signal[10] <= delay_w[4200];
+signal[11] <= delay_w[3770];
+signal[12] <= delay_w[3340];
+signal[13] <= delay_w[2910];
+signal[14] <= delay_w[2480];
+signal[15] <= delay_w[2050];
+signal[16] <= delay_w[1620];
+signal[17] <= delay_w[1190];
+signal[18] <= delay_w[760];
+signal[19] <= delay_w[330];
+
+//delay for angle -25
+signal[0] <= delay_w[8500];
+signal[1] <= delay_w[8141];
+signal[2] <= delay_w[7782];
+signal[3] <= delay_w[7423];
+signal[4] <= delay_w[7064];
+signal[5] <= delay_w[6705];
+signal[6] <= delay_w[6346];
+signal[7] <= delay_w[5987];
+signal[8] <= delay_w[5628];
+signal[9] <= delay_w[5269];
+signal[10] <= delay_w[4910];
+signal[11] <= delay_w[4551];
+signal[12] <= delay_w[4192];
+signal[13] <= delay_w[3833];
+signal[14] <= delay_w[3474];
+signal[15] <= delay_w[3115];
+signal[16] <= delay_w[2756];
+signal[17] <= delay_w[2397];
+signal[18] <= delay_w[2038];
+signal[19] <= delay_w[1679];
+
+//delay for angle -20
+signal[0] <= delay_w[8500];
+signal[1] <= delay_w[8213];
+signal[2] <= delay_w[7926];
+signal[3] <= delay_w[7639];
+signal[4] <= delay_w[7352];
+signal[5] <= delay_w[7065];
+signal[6] <= delay_w[6778];
+signal[7] <= delay_w[6491];
+signal[8] <= delay_w[6204];
+signal[9] <= delay_w[5917];
+signal[10] <= delay_w[5630];
+signal[11] <= delay_w[5343];
+signal[12] <= delay_w[5056];
+signal[13] <= delay_w[4769];
+signal[14] <= delay_w[4482];
+signal[15] <= delay_w[4195];
+signal[16] <= delay_w[3908];
+signal[17] <= delay_w[3621];
+signal[18] <= delay_w[3334];
+signal[19] <= delay_w[3047];
+
+//delay for angle -15
+signal[0] <= delay_w[8500];
+signal[1] <= delay_w[8285];
+signal[2] <= delay_w[8070];
+signal[3] <= delay_w[7855];
+signal[4] <= delay_w[7640];
+signal[5] <= delay_w[7425];
+signal[6] <= delay_w[7210];
+signal[7] <= delay_w[6995];
+signal[8] <= delay_w[6780];
+signal[9] <= delay_w[6565];
+signal[10] <= delay_w[6350];
+signal[11] <= delay_w[6135];
+signal[12] <= delay_w[5920];
+signal[13] <= delay_w[5705];
+signal[14] <= delay_w[5490];
+signal[15] <= delay_w[5275];
+signal[16] <= delay_w[5060];
+signal[17] <= delay_w[4845];
+signal[18] <= delay_w[4630];
+signal[19] <= delay_w[4415];
+
+//delay for angle -10
+signal[0] <= delay_w[8500];
+signal[1] <= delay_w[8357];
+signal[2] <= delay_w[8214];
+signal[3] <= delay_w[8071];
+signal[4] <= delay_w[7928];
+signal[5] <= delay_w[7785];
+signal[6] <= delay_w[7642];
+signal[7] <= delay_w[7499];
+signal[8] <= delay_w[7356];
+signal[9] <= delay_w[7213];
+signal[10] <= delay_w[7070];
+signal[11] <= delay_w[6927];
+signal[12] <= delay_w[6784];
+signal[13] <= delay_w[6641];
+signal[14] <= delay_w[6498];
+signal[15] <= delay_w[6355];
+signal[16] <= delay_w[6212];
+signal[17] <= delay_w[6069];
+signal[18] <= delay_w[5926];
+signal[19] <= delay_w[5783];
+
+//delay for angle -5
+signal[0] <= delay_w[8500];
+signal[1] <= delay_w[8428];
+signal[2] <= delay_w[8356];
+signal[3] <= delay_w[8284];
+signal[4] <= delay_w[8212];
+signal[5] <= delay_w[8140];
+signal[6] <= delay_w[8068];
+signal[7] <= delay_w[7996];
+signal[8] <= delay_w[7924];
+signal[9] <= delay_w[7852];
+signal[10] <= delay_w[7780];
+signal[11] <= delay_w[7708];
+signal[12] <= delay_w[7636];
+signal[13] <= delay_w[7564];
+signal[14] <= delay_w[7492];
+signal[15] <= delay_w[7420];
+signal[16] <= delay_w[7348];
+signal[17] <= delay_w[7276];
+signal[18] <= delay_w[7204];
+signal[19] <= delay_w[7132];
+
+
+//delay code for positive angles
+
+//delay for angle 0
+signal[0] <= delay_w[0];
+signal[1] <= delay_w[0];
+signal[2] <= delay_w[0];
+signal[3] <= delay_w[0];
+signal[4] <= delay_w[0];
+signal[5] <= delay_w[0];
+signal[6] <= delay_w[0];
+signal[7] <= delay_w[0];
+signal[8] <= delay_w[0];
+signal[9] <= delay_w[0];
+signal[10] <= delay_w[0];
+signal[11] <= delay_w[0];
+signal[12] <= delay_w[0];
+signal[13] <= delay_w[0];
+signal[14] <= delay_w[0];
+signal[15] <= delay_w[0];
+signal[16] <= delay_w[0];
+signal[17] <= delay_w[0];
+signal[18] <= delay_w[0];
+signal[19] <= delay_w[0];
+
+//delay for angle 5
+signal[0] <= delay_w[0];
+signal[1] <= delay_w[72];
+signal[2] <= delay_w[144];
+signal[3] <= delay_w[216];
+signal[4] <= delay_w[288];
+signal[5] <= delay_w[360];
+signal[6] <= delay_w[432];
+signal[7] <= delay_w[504];
+signal[8] <= delay_w[576];
+signal[9] <= delay_w[648];
+signal[10] <= delay_w[720];
+signal[11] <= delay_w[792];
+signal[12] <= delay_w[864];
+signal[13] <= delay_w[936];
+signal[14] <= delay_w[1008];
+signal[15] <= delay_w[1080];
+signal[16] <= delay_w[1152];
+signal[17] <= delay_w[1224];
+signal[18] <= delay_w[1296];
+signal[19] <= delay_w[1368];
+
+//delay for angle 10
+signal[0] <= delay_w[0];
+signal[1] <= delay_w[143];
+signal[2] <= delay_w[286];
+signal[3] <= delay_w[429];
+signal[4] <= delay_w[572];
+signal[5] <= delay_w[715];
+signal[6] <= delay_w[858];
+signal[7] <= delay_w[1001];
+signal[8] <= delay_w[1144];
+signal[9] <= delay_w[1287];
+signal[10] <= delay_w[1430];
+signal[11] <= delay_w[1573];
+signal[12] <= delay_w[1716];
+signal[13] <= delay_w[1859];
+signal[14] <= delay_w[2002];
+signal[15] <= delay_w[2145];
+signal[16] <= delay_w[2288];
+signal[17] <= delay_w[2431];
+signal[18] <= delay_w[2574];
+signal[19] <= delay_w[2717];
+
+//delay for angle 15
+signal[0] <= delay_w[0];
+signal[1] <= delay_w[215];
+signal[2] <= delay_w[430];
+signal[3] <= delay_w[645];
+signal[4] <= delay_w[860];
+signal[5] <= delay_w[1075];
+signal[6] <= delay_w[1290];
+signal[7] <= delay_w[1505];
+signal[8] <= delay_w[1720];
+signal[9] <= delay_w[1935];
+signal[10] <= delay_w[2150];
+signal[11] <= delay_w[2365];
+signal[12] <= delay_w[2580];
+signal[13] <= delay_w[2795];
+signal[14] <= delay_w[3010];
+signal[15] <= delay_w[3225];
+signal[16] <= delay_w[3440];
+signal[17] <= delay_w[3655];
+signal[18] <= delay_w[3870];
+signal[19] <= delay_w[4085];
+
+//delay for angle 20
+signal[0] <= delay_w[0];
+signal[1] <= delay_w[287];
+signal[2] <= delay_w[574];
+signal[3] <= delay_w[861];
+signal[4] <= delay_w[1148];
+signal[5] <= delay_w[1435];
+signal[6] <= delay_w[1722];
+signal[7] <= delay_w[2009];
+signal[8] <= delay_w[2296];
+signal[9] <= delay_w[2583];
+signal[10] <= delay_w[2870];
+signal[11] <= delay_w[3157];
+signal[12] <= delay_w[3444];
+signal[13] <= delay_w[3731];
+signal[14] <= delay_w[4018];
+signal[15] <= delay_w[4305];
+signal[16] <= delay_w[4592];
+signal[17] <= delay_w[4879];
+signal[18] <= delay_w[5166];
+signal[19] <= delay_w[5453];
+
+//delay for angle 25
+signal[0] <= delay_w[0];
+signal[1] <= delay_w[359];
+signal[2] <= delay_w[718];
+signal[3] <= delay_w[1077];
+signal[4] <= delay_w[1436];
+signal[5] <= delay_w[1795];
+signal[6] <= delay_w[2154];
+signal[7] <= delay_w[2513];
+signal[8] <= delay_w[2872];
+signal[9] <= delay_w[3231];
+signal[10] <= delay_w[3590];
+signal[11] <= delay_w[3949];
+signal[12] <= delay_w[4308];
+signal[13] <= delay_w[4667];
+signal[14] <= delay_w[5026];
+signal[15] <= delay_w[5385];
+signal[16] <= delay_w[5744];
+signal[17] <= delay_w[6103];
+signal[18] <= delay_w[6462];
+signal[19] <= delay_w[6821];
+
+//delay for angle 30
+signal[0] <= delay_w[0];
+signal[1] <= delay_w[430];
+signal[2] <= delay_w[860];
+signal[3] <= delay_w[1290];
+signal[4] <= delay_w[1720];
+signal[5] <= delay_w[2150];
+signal[6] <= delay_w[2580];
+signal[7] <= delay_w[3010];
+signal[8] <= delay_w[3440];
+signal[9] <= delay_w[3870];
+signal[10] <= delay_w[4300];
+signal[11] <= delay_w[4730];
+signal[12] <= delay_w[5160];
+signal[13] <= delay_w[5590];
+signal[14] <= delay_w[6020];
+signal[15] <= delay_w[6450];
+signal[16] <= delay_w[6880];
+signal[17] <= delay_w[7310];
+signal[18] <= delay_w[7740];
+signal[19] <= delay_w[8170];
+
---
+++ generate_delay_code.m
@@ -0,0 +1,34 @@
+%parameters
+%units in mm, s
+fclk = 50E6; %fpga freq
+vs = 340E3; % speed of sound in air
+d = 5.588; %spacing between consecutive columns
+angles = [0 5 10 15 20 25 30];
+t_ud = d * sind(angles / vs);
+sr_ud = round(t_ud * fclk);
+neg_angles = fliplr(angles);
+neg_sr_ud = fliplr(sr_ud);
+
+fid = fopen('delay_code.v', 'w+');
+fprintf(fid, '//delay code for negative angles\n\n');
+
+%genereate delay code for negative angles
+for i = 1:6
+  fprintf(fid, '//delay for angle -%d\n', neg_angles(i));
+  for j = 0:19
+    ind = 8500 - j * neg_sr_ud(i);
+    fprintf(fid, 'signal[%d] <= delay_w[%d];\n', j, ind);
+  end
+  fprintf(fid, '\n');
+end 
+
+fprintf(fid, '\n//delay code for positive angles\n\n');
+for i = 1:7
+  fprintf(fid, '//delay for angle %d\n', angles(i));
+  for j = 0:19
+    ind = j * sr_ud(i);
+    fprintf(fid, 'signal[%d] <= delay_w[%d];\n', j, ind);
+  end
+  fprintf(fid, '\n');
+end 
+
